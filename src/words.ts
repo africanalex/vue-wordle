936763a4969c26d866e7d0d68f8e428785e045a6
africanalex <africanalex@outlook.com>
2022-02-26 10:30:12 +0200

    random word

--- src/words.ts
+++ src/words.ts
@@ -14,13 +14,13 @@ export function getWordOfTheDay() {
     }
   }
 
-  const now = new Date()
-  const start = new Date(2022, 0, 0)
-  const diff = Number(now) - Number(start)
-  let day = Math.floor(diff / (1000 * 60 * 60 * 24))
-  while (day > answers.length) {
-    day -= answers.length
-  }
+  //const now = new Date()
+  //const start = new Date(2022, 0, 0)
+  //const diff = Number(now) - Number(start)
+  let day = Math.floor(Math.random() * answers.length)
+  //while (day > answers.length) {
+  //  day -= answers.length
+  //}
   return answers[day]
 }
 
